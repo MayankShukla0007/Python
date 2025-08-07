5b6ebebb2c53c8578c1543eac04414766d0d8271
MayankShukla0007 <shuklasmayank7@gmail.com>
2025-08-07 10:01:46 +0000

    done of code

---
+++ 02_program_variable_datatypes.py
@@ -0,0 +1,22 @@
+Name = "Mayank @Shukla"
+age = 20
+price = 25.99
+
+# print("My name is: ", Name)
+# print("My age is: ", age)
+
+print(type (Name))
+print(type (age))
+print(type (price))
+
+Name2 = 'Mayank'
+Name3 = '''Mayank'''
+
+# print(Name2)
+# print(Name3)
+
+old = True
+a = None
+
+print(type (old))
+print(type (a))
